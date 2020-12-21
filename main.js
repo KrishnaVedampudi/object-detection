4943e9cc8f84480cf7631c71d9e90a5a0041c557
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-12-21 21:40:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 status = "";
 objects=[];
-
 function setup()
 {
     canvas = createCanvas(380, 380);
@@ -15,21 +14,19 @@ function setup()
 }
 function modelLoaded()
 {
-    console.log('Model Loaded!');    
-    document.getElementsByClassName('body').style.opacity = "1.0";
+    console.log('Model Loaded!');       
     objectDetector.detect(video, gotResults);
-    document.getElementById('status').innerHTML = "Status : Detecting objects";
-    status = true;     
-    document.getElementById("loader").style.display = "none";   
+    document.getElementById('status').innerHTML = "Status : Detecting objects";       
 }
 function gotResults(error, results)
 {
    if(error)
    {
-       console.log(error);
+       throw new Error('It was an error!!!');    
    }else{
+       status = true; 
        console.log(results);
-       objects = results;              
+       objects = results;                        
    }
 }
 function draw()
@@ -37,20 +34,24 @@ function draw()
     image(video, 0, 0, 380, 380);    
     if(status != "")
     {
+        if(objects != null){
       for(i=0; i <= objects.length; i++)
        {        
         r = random(255);
         g = random(255);
         b = random(255);
         textSize(18);
+        document.getElementById('body').style.opacity = "0.9";
         document.getElementById("status").innerHTML = "Status : Objects Detetcted";
+        document.getElementById("loader").style.display = "none";   
         document.getElementById("noo").innerHTML = "Number Of Objects Detected : "+ objects.length;
         fill(r, g, b);
-        percent = floor(objects[i].confidence*100);
+        percent = floor(objects[i].confidence * 100);
         text(objects[i].label + " " + percent + "%", objects[i].x +15, objects[i].y+15);
         noFill();
         stroke(r, g, b);
         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
        } 
+    }
     }        
 }
